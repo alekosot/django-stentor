1243862d0558fec9b6c8a9cfb6748fb3e9213827
Alex Sotiriou <a.sotiriou@difio.gr>
2017-01-13 16:05:33 +0200

    Subscribe and unsubscribe forms.

--- stentor/forms.py
+++ stentor/forms.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from datetime import datetime
-
 from django import forms
-from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
-from stentor import utils as stentor_utils
+from stentor.utils import obfuscator, subscribe
 
 from .models import Subscriber
 
@@ -17,12 +14,12 @@ class SubscribeForm(forms.Form):
     A form for subscribing to the newsletter to be used in the frontend as a
     simple public form.
     """
+    email = forms.EmailField()
 
     def save(self):
         email = self.cleaned_data['email']
-        subscriber = Subscriber.objects.get_or_create(email=email)[0]
-        subscriber.is_unsubscribed = True
-        return subscriber.save()
+        subscriber = subscribe(email)
+        return subscriber
 
 
 class UnsubscribeForm(forms.Form):
@@ -42,31 +39,26 @@ class UnsubscribeForm(forms.Form):
         fields = ('unsubscribe_hash', 'email')
 
     def clean(self):
-        cleaned_data = self.cleaned_data
+        cleaned_data = super(UnsubscribeForm, self).clean()
+
+        # Skip this, if the email field did not survive previous validation
+        if 'email' not in cleaned_data:
+            return cleaned_data
+
         unsubscribe_hash = cleaned_data['unsubscribe_hash']
 
-        _request_day, subscribed_day, pk = stentor_utils.hasher.decode(
+        __, subscriber_pk = obfuscator.decode_unsubscribe_hash(
             unsubscribe_hash
         )
 
-        now = datetime.now()
-        request_day = datetime.fromordinal(_request_day)
-        if (now - request_day).days > 3:
-            raise forms.ValidationError(_(
-                'It has been more than 3 days since the unsubscribe request '
-                'was made. Please make a new request.'
-            ))
-
-        subscribed_day = timezone.fromordinal(subscribed_day)
         try:
-            subscriber = Subscriber.objects.get(pk=pk)
+            subscriber = Subscriber.objects.get(pk=subscriber_pk)
         except Subscriber.DoesNotExist:
             raise forms.ValidationError(_(
-                'There is no subscription with this email address.'
+                'There is no subscriber with this email address registered.'
             ))
 
-        if subscriber.creation_date.day != subscribed_day \
-                or subscriber.get_email() != cleaned_data['email']:
+        if subscriber.get_email() != cleaned_data['email']:
             raise forms.ValidationError(_(
                 'The web address you used to reach this form is incorrect. '
                 'Please check that you have copied it correctly.'
