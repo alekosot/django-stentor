b51e5cf3e3e292e9b57f84d03067004c3d331a2b
Alex Sotiriou <a.sotiriou@difio.gr>
2017-01-17 14:26:42 +0200

    Squashed commits for admin. Thanks dimkoug for the work on this.

--- stentor/admin.py
+++ stentor/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
 
-# Register your models here.
+from .actions import (
+    send_keep_scheduled_email, send_delete_scheduled_email, schedule_sending)
+
+from .models import (
+    MailingList, Subscriber, Newsletter, ScheduledSending
+)
+
+
+class MailingListAdmin(admin.ModelAdmin):
+
+    fields = ('name',)
+    list_display = ('name',)
+
+
+class SubscriberAdmin(admin.ModelAdmin):
+
+    fields = ('user', 'email', 'mailing_lists', 'is_active',)
+    list_display = ('user', 'email', 'is_active',)
+    list_filter = ('is_active',)
+
+
+class NewsletterAdmin(admin.ModelAdmin):
+
+    fields = ('subject', 'template', 'mailing_lists', 'subscribers', 'slug')
+    list_display = ('subject', 'slug', 'email_views', 'web_views')
+    actions = [schedule_sending]
+
+
+class ScheduledSendingAdmin(admin.ModelAdmin):
+
+    fields = ('subscriber', 'newsletter', 'sending_date', 'message')
+    list_display = ('subscriber', 'newsletter', 'sending_date', 'message')
+    actions = [send_keep_scheduled_email, send_delete_scheduled_email]
+
+
+admin.site.register(MailingList, MailingListAdmin)
+admin.site.register(Subscriber, SubscriberAdmin)
+admin.site.register(Newsletter, NewsletterAdmin)
+admin.site.register(ScheduledSending, ScheduledSendingAdmin)
