ea8c81fded147bfa1980b48e06628699a3ea2a57
Alex Sotiriou <a.sotiriou@difio.gr>
2017-12-21 17:54:02 +0200

    Refactor of template logic and default templates.

--- stentor/templatetags/stentor_tags.py
+++ stentor/templatetags/stentor_tags.py
@@ -2,10 +2,13 @@
 from __future__ import unicode_literals
 
 from django import template
+from django.contrib.sites.models import Site
 from django.template.loader import render_to_string
 
 
 register = template.Library()
+
+current_site = Site.objects.get_current()
 
 
 # This does not use an inclusion tag, so we can ensure that the output does not
@@ -13,6 +16,8 @@ register = template.Library()
 # code editor of the end user that may enforce a trailing whitespace).
 @register.simple_tag(takes_context=True)
 def web_view_url(context):
+    if 'stentor_domain' not in context:
+        context['stentor_domain'] = current_site.domain
     html = render_to_string('stentor/partials/web_view_url.html', context)
     return html.strip()
 
@@ -22,10 +27,15 @@ def web_view_url(context):
 # code editor of the end user that may enforce a trailing whitespace).
 @register.simple_tag(takes_context=True)
 def email_tracker_url(context):
+    if 'stentor_domain' not in context:
+        context['stentor_domain'] = current_site.domain
     html = render_to_string('stentor/partials/email_tracker_url.html', context)
     return html.strip()
 
 
 @register.simple_tag(takes_context=True)
 def unsubscribe_url(context):
-    return context['subscriber'].get_unsubscribe_url()
+    if 'stentor_domain' not in context:
+        context['stentor_domain'] = current_site.domain
+    html = render_to_string('stentor/partials/unsubscribe_url.html', context)
+    return html.strip()
