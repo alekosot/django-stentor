b53ef2ebd66e5ddb78cee856222b3a7329e9824c
Alex Sotiriou <a.sotiriou@difio.gr>
2018-04-25 17:58:20 +0300

    Added forgotten import in template tags.

--- stentor/templatetags/stentor_tags.py
+++ stentor/templatetags/stentor_tags.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django import template
+from django.core.exceptions import ImproperlyConfigured
 from django.template.loader import render_to_string
 from django.utils import six
 
