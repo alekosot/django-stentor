7f54fce6493b652681afa1be3ebcba5e08a0aa17
Alex Sotiriou <alxs@protonmail.com>
2019-05-24 18:46:36 +0300

    Make subscribe respect SUBSCRIPTION_CONFIRMATION

--- stentor/utils.py
+++ stentor/utils.py
@@ -46,9 +46,13 @@ def subscribe(email, add_to_default=True, mailing_lists=None, subscriber=None):
     from .models import Subscriber, MailingList
 
     if not subscriber:
-        subscriber, __ = Subscriber.objects.get_or_create(email=email)
+        subscriber, created = Subscriber.objects.get_or_create(email=email)
 
-    if not subscriber.is_active:
+    # Mark the subscriber as active:
+    # - if it's a new subscriber and no subscription confirmation is needed
+    # - if it's an existing subscriber and not already active
+    if (created and not stentor_conf.SUBSCRIPTION_CONFIRMATION)
+            or (not created and not subscriber.is_active):
         subscriber.is_active = True
         subscriber.save()
 
